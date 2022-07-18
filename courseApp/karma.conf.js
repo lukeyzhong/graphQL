8f4d48bc44d404006ed2b4ac154e47e02158cfab
lukeyzhong <lukeyzhong@gmail.com>
2022-07-18 10:47:29 -0500

    update

--- courseApp/karma.conf.js
+++ courseApp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/space-x'),
+      dir: require('path').join(__dirname, './coverage/course-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
